b259da51410cfcd4dc625bc9a40f681544ff3ce6
Cameron <cameronadevereaux@gmail.com>
2023-09-28 13:10:19 -0700

    Revert "Merge https://github.com/emilk/eframe_template"
    This reverts commit d578d0ebdc07b8e9295c4f725c603c69c87fea08, reversing
    changes made to d93a1e2dc17da49e02875bc288b4a983cfc7db40.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,33 @@
-#![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+use lopdf::{Document, Error};
+use pdf_tools::{
+    pdf_outline::print_outline, PdfFontReader, PdfOutlineGenerator, PdfOutlineInserter,
+};
 
-// When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
-fn main() -> eframe::Result<()> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+mod pdf_tools;
 
-    let native_options = eframe::NativeOptions {
-        initial_window_size: Some([400.0, 300.0].into()),
-        min_window_size: Some([300.0, 220.0].into()),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "eframe template",
-        native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
-    )
-}
+fn main() -> Result<(), Error> {
+    let mut doc = Document::load("test/abop.pdf")?;
 
-// When compiling to web using trunk:
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    // Redirect `log` message to `console.log` and friends:
-    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+    let mut fonts = doc.get_all_fonts().unwrap();
 
-    let web_options = eframe::WebOptions::default();
+    let mut outline_fonts = vec![];
+    while let Some((font, _count)) = fonts.pop_first() {
+        outline_fonts.push(font);
+    }
 
-    wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
-            .start(
-                "the_canvas_id", // hardcode it
-                web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
-            )
-            .await
-            .expect("failed to start eframe");
-    });
+    let heading_fonts = vec![
+        outline_fonts[0].clone(),
+        outline_fonts[5].clone(),
+        outline_fonts[12].clone(),
+    ];
+
+    let outline = doc.generate_outline(&heading_fonts);
+
+    print_outline(&outline);
+
+    doc.insert_outline(&outline)?;
+
+    doc.save("./test/test.pdf")?;
+
+    Ok(())
 }
